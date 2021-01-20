0f88d03389b4fe9e149a1820fcfc74c6bb870620
Arash <arashm81@gmail.com>
2021-01-20 12:33:31 -0800

    removed unrelated comments

--- invJacobianTensor.m
+++ invJacobianTensor.m
@@ -8,11 +8,6 @@ function [dets, invJe] = invJacobianTensor(elemJac, P)
  idx = reshape(reshape(1:r,blk,c)',[],1);
  
  permutedElemJac = elemJac(idx,:);
- 
- %make 2D array to 3D:
-%  sub = [1 2 3; 4 5 6; 7 8 9]
-%  m = repmat(sub,8,1)
- %permute(reshape(permute(m,[2,1,3]),3,3,[]),[2,1,3])
   
  for i = 1:blk
      tmp=permutedElemJac((i-1)*c+1:i*c,:);
