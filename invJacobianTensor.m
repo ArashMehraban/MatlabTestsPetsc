d3751b527e706ed00ad100f2075714fd908b5963
Arash <arashm81@gmail.com>
2021-01-23 16:45:23 -0800

    iterlaced

--- invJacobianTensor.m
+++ invJacobianTensor.m
@@ -1,17 +1,13 @@
-function [dets, invJe] = invJacobianTensor(elemJac)
+function [dets, invJe] = invJacobianTensor(dx)
 
- [r,c] = size(elemJac);
+ [r,c] = size(dx);
  invJe = zeros(r,c);
  blk = r/c;
  dets = zeros(blk,1);
- 
- idx = reshape(reshape(1:r,blk,c)',[],1);
- 
- permutedElemJac = elemJac(idx,:);
-  
+
  for i = 1:blk
-     tmp=permutedElemJac((i-1)*c+1:i*c,:);
-     invJe(idx((i-1)*c+1:i*c),:) =inv(tmp);
+     tmp=dx((i-1)*c+1:i*c,:);
+     invJe((i-1)*c+1:i*c,:) =inv(tmp);
      dets(i) = det(tmp);
  end
  
