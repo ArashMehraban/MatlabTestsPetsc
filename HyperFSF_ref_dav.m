7d8aeb1c8c54c62022678a95a0661ac004c04e15
Arash <arashm81@gmail.com>
2021-01-26 17:01:04 -0800

    userf for residual eval

---
+++ HyperFSF_ref_dav.m
@@ -0,0 +1,73 @@
+function [usrfStored, f1] = HyperFSF_ref_dav(du, dXdx, wdetj, phys)
+%HyperFSF_ref implements constitutive model for the compressible Neo-Hookean
+%Hyperelasticity at finite strain using the reference configuration 
+%
+% IMPORTANT:
+%
+%              [du1/dx | du2/dx | du3/dx]    <-for quadrature 1 
+%              [du1/dy | du2/dy | du3/dy]    <-for quadrature 1
+%              [du1/dz | du2/dz | du3/dz]    <-for quadrature 1
+%            ------------------------------
+%              [du1/dx | du2/dx | du3/dx]    <-for quadrature 2 
+%              [du1/dy | du2/dy | du3/dy]    <-for quadrature 2
+%              [du1/dz | du2/dz | du3/dz]    <-for quadrature 2
+%            ------------------------------
+%              [.....  | .....  |  .....]           .
+%              [.....  | .....  |  .....]           .
+%              [.....  | .....  |  .....]           .
+%            ------------------------------
+%              [.....  | .....  |  .....]           .
+%              [.....  | .....  |  .....]           .                  
+%      du =    [.....  | .....  |  .....]           .
+%            ------------------------------
+%              [.....  | .....  |  .....]
+%              [.....  | .....  |  .....]
+%              [.....  | .....  |  .....]
+%            ------------------------------
+%              [.....  | .....  |  .....]
+%              [.....  | .....  |  .....]
+%              [.....  | .....  |  .....]
+%            ------------------------------
+%              [.....  | .....  |  .....]
+%              [.....  | .....  |  .....]
+%              [.....  | .....  |  .....]
+%            ------------------------------
+%              [.....  | .....  |  .....]          .
+%              [.....  | .....  |  .....]          .
+%              [.....  | .....  |  .....]          .
+%   
+%
+%   du and dXdx have the same layout
+%
+    
+   nu = phys.nu;
+   E = phys.E;
+   TwoMu=E/(1+nu);
+   muu = TwoMu /2;
+   Kbulk=E/(3*(1-2*nu));
+   lambda=(3*Kbulk-TwoMu)/3;
+
+   Gradu=  0*du;      %Allocate sapce for gradu computed here
+   usrfStored = 0*du; %Allocate space for usrfStored as output for HyperFSF_dF_dav 
+   f1 = 0*du;         %f1 is dvdX = dXdx * tau * wdetj (libCEED notation)
+   J = 0*wdetj;       %Allocate space for J = det(F)
+   [r,c] = size(du);
+   blk = r/c; 
+   
+   I3=eye(3); 
+   
+   for i = 1:blk
+     du_tmp= dXdx((i-1)*c+1:i*c,:) * du((i-1)*c+1:i*c,:); %ref config
+     Gradu((i-1)*c+1:i*c,:) = du_tmp; %ref config
+     F = eye(3) + du_tmp;             % dx_current/dx_initial
+     %dX_ref/dX_current = (dX_ref/dx_initial) (dx_initial/dx_current)
+     dX_ref_dx_cur = dXdx((i-1)*c+1:i*c,:) * F\I3; %May want to store this
+%      usrfStored((i-1)*c+1:i*c,:) = dX_ref_dx_cur;
+     usrfStored((i-1)*c+1:i*c,:) = F;
+     J(i) = det(F);
+     b = F*F';
+     tau = (muu*b + (lambda*log(J(i))-muu)*I3)/J(i);
+     f1((i-1)*c+1:i*c,:) = dX_ref_dx_cur'* tau * wdetj(i);
+   end
+       
+end
