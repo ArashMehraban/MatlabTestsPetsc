b3c877118e84af28714dfb84a3474c7bc582c961
Arash <arashm81@gmail.com>
2021-02-04 20:16:26 -0800

    bend by 25%

--- bc_bend.m
+++ bc_bend.m
@@ -1,7 +1,7 @@
 function boundary_dof = bc_bend(boundary_dof, boundary_vtx, load_increment, AppCtx)
   
-    
-  boundary_dof(:,1) = -0.1*load_increment; % 50% the size of the beam in the x-coords bent in -Y direction (beam_8e_l999_r998_6ss.png)  
-
+  %boundary_dof(:,1) = -0.3*load_increment;  
+  boundary_dof(:,2) = -2*load_increment; % 50% the size of the beam in the x-coords bent in -Y direction (beam_8e_l999_r998_6ss.png)  
+  %boundary_dof(:,3) = -0.3*load_increment;
 
 end
